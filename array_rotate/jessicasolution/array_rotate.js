b0f0b3e15dd3378a4dad9e860fa6e9238483a010
Jessica Morrell <jessica.ann.morrell@gmail.com>
2018-06-27 22:29:52 -0600

    adding solutions for array_rotate and balanced_delimiter

---
+++ array_rotate/jessicasolution/array_rotate.js
@@ -0,0 +1,12 @@
+
+function rotateArray(arr, steps) {
+    var sliceIndex = (arr.length)-steps;
+    
+    //slice makes a copy of the array, or else this wouldn't work
+    var firstHalf = arr.slice(sliceIndex, (arr.length));
+    var secondHalf = arr.slice(0, (sliceIndex));
+  
+    return firstHalf.concat(secondHalf);
+  }
+  
+  rotateArray([1, 2, 3, 4, 5], 3);
